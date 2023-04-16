49739f353920dbaa0ddb45c61d67acbb9702ef09
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2023-04-16 07:41:56 +0300

    Login

--- cypress/e2e/Login.spec.cy.js
+++ cypress/e2e/Login.spec.cy.js
@@ -7,6 +7,14 @@ describe('Cypress Selector Playground', ()=>{
                     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123");  
                     
                     cy.get('.oxd-button').click();
-          });     
+          });   
+          
+          it.only('Forgot Password', ()=>{
+
+                    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+                    cy.contains('Forgot').click();
+                    
+          })
 
 });
