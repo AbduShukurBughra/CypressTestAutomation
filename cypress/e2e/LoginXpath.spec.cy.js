a8b0da3f2ebe02821d5e1488ff56761751840f94
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2023-04-16 22:02:05 +0300

    xpath and get

--- cypress/e2e/LoginXpath.spec.cy.js
+++ cypress/e2e/LoginXpath.spec.cy.js
@@ -2,11 +2,15 @@ describe('Cypress Selector Playground', ()=>{
           it.only('Login Test', ()=>{
                     cy.visit("https://opensource-demo.orangehrmlive.com/");
 
-                    cy.xpath("//input[@name='username']").type("admin");
+                    // cy.xpath("//input[@name='username']").type("admin");
+                    cy.get("input[name='username']").type("admin");
 
-                    cy.xpath("//input[@name='password']").type("admin123");
+                    // cy.xpath("//input[@name='password']").type("admin123");
+                    cy.get("input[name='password']").type("admin123");
                     
-                    cy.xpath("//button[@type='submit']").click();
+                    // cy.xpath("//button[@type='submit']").click();
+                    cy.get("button[type='submit']").click();
+                    // cy.get('.oxd-button').click();
           });   
           
 
--- cypress/e2e/Registration.spec.cy.js
+++ cypress/e2e/Registration.spec.cy.js
@@ -6,7 +6,7 @@ describe('Fill all details', ()=>{
                     cy.get('#firstname').type("Bughra");
                     cy.get('#lastname').type("Ali");
                     cy.get('#userName').type("Hakan");
-                    cy.get('#password').type("admin123!");
+                    cy.get('#password').type("Admin123!");
                     cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click();
                     cy.get('#register').click();
                     // cy.xpath("//input[@id='firstname']").type("Bughra");
