06fbe9822c3e6fbca77a748d52bc925a32731e03
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2023-04-16 06:06:25 +0300

    cypress demo

---
+++ cypress/e2e/Login.spec.cy.js
@@ -0,0 +1,12 @@
+describe('Cypress Selector Playground', ()=>{
+          it('Login Test', ()=>{
+                    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+                    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin");
+
+                    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123");  
+                    
+                    cy.get('.oxd-button').click();
+          });     
+
+});
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,10 @@
+describe("First Test Suite",()=>{
+  it("First Test",()=>{
+         expect(true).to.equal(true);
+  });
+
+  it("Second Test",()=>{
+         expect(true).to.equal(true);
+  });
+
+})
